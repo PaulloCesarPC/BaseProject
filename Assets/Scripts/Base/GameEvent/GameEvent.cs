ab3b19e77a391fc4d2f70ab0bf5b80067e852229
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-02-27 11:24:43 -0300

    Bootstrap removido, SmartDebug e GameEvent

---
+++ Assets/Scripts/Base/GameEvent/GameEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameEvent : ScriptableObject
+{
+    /// <summary>
+    /// The list of listeners that this event will notify if it is invoked.
+    /// </summary>
+    private readonly List<GameEventListener> eventListeners =
+        new List<GameEventListener>();
+
+    /// <summary>
+    /// The list of listeners that this event will notify if it is invoked.
+    /// </summary>
+    private readonly List<Action> actions = new List<Action>();
+
+    public void Invoke()
+    {
+        for (int i = actions.Count - 1; i >= 0; i--)
+            actions[i].Invoke();
+
+        for (int i = eventListeners.Count - 1; i >= 0; i--)
+            eventListeners[i].OnEventRaised();
+    }
+
+    public void AddListener(GameEventListener listener)
+    {
+        if (!eventListeners.Contains(listener))
+            eventListeners.Add(listener);
+    }
+
+    public void RemoveListener(GameEventListener listener)
+    {
+        if (eventListeners.Contains(listener))
+            eventListeners.Remove(listener);
+    }
+
+    public void AddListener(Action action)
+    {
+        if (!actions.Contains(action))
+            actions.Add(action);
+    }
+
+    public void RemoveListener(Action action)
+    {
+        if (actions.Contains(action))
+            actions.Remove(action);
+    }
+}
---
+++ Assets/Scripts/Base/GameEvent/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [Tooltip("Event to register with.")]
+    public GameEvent Event;
+
+    [Tooltip("Response to invoke when Event is raised.")]
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        Event.AddListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.RemoveListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+}
